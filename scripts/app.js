b6dcd59455d1c04ab6ad42e0c0a82d20ec256a0a
Lani Field <apprentice@StMac3-2.local>
2015-08-06 20:43:36 +1200

    add collision detection. Spawn in random obsticals

--- scripts/app.js
+++ scripts/app.js
@@ -5,6 +5,7 @@ $(document).ready(function() {
   playerAlive = true
   var i = 0
   var speedFactor = 3
+  var spawnCount = 10
 
   // Have to communicate on class names, so that it is styled by the CSS
 
@@ -21,9 +22,8 @@ $(document).ready(function() {
       <div class='road'></div>\
       <div class='side_line'></div>\
       <div class='grass'></div>\
-
-      <div class='car_slider'><img id='avatar' src='images/broombroomcar.png'></div> <br><br>\
-      <h1 id='scoreboard'><br>Score:</h1>" );
+      <div class='car_slider'><img id='avatar' src='images/broombroomcar.png'></div> <br><br>");
+     $(".board").append("<h1 id='scoreboard'><br>Score:</h1>" );
 
 
     if(i%100==0){$(".side_line").append(
@@ -35,7 +35,9 @@ $(document).ready(function() {
 Board()
 
   function doMove() {
+
     if(!playerAlive){
+
       return;
     }
      // $(".moving").css("margin-top", ((i/10)+"%"));
@@ -48,7 +50,7 @@ Board()
     });
     setTimeout(doMove,30);
     i= i+1
-    if(playerPostion> 400){
+    if(playerPostion> 800){
       playerAlive=false
     }
     if(i%30==0){$(".side_line").append(
@@ -58,17 +60,37 @@ Board()
       "<div class='centre-line-stripe moving' data-value='"+i+"'></div>").css('margin-top', '-180px')
     }
 
+    if(spawnCount==0){
+        // var randnum =
+        $($(".road")[0]).append(
+        "<img class='obstacle moving' data-value='"+i+"' src='images/fwog.png' style='margin-left:" + Math.floor((Math.random() * 400) + 30) + "px'>");
+
+        spawnCount = Math.floor((Math.random() * 70) + 30)
+      // css({'margin-top' : '0px' , 'margin-left': Math.floor((Math.random() * 400) + 30)})
+    }
+    var carPos = (parseInt($("#avatar").css("margin-left")))
+    $(".obstacle").each(function(){
+      if(parseInt($(this).css("margin-top"))>320 &&parseInt($(this).css("margin-top"))<440 ){
+        console.log($(this).css("margin-left"))
+        if(parseInt($(this).css("margin-left"))>(carPos-20) && parseInt($(this).css("margin-left"))<(carPos+20) ) {
+          alert("GAME OVER. score = " +parseInt((i/10)))
+          playerAlive = false
+
+        }
+      }
+    });
+    spawnCount = spawnCount-1
   }
 
   doMove()
-  $(document).keydown(function(event){
-    if ((parseInt($(avatar).css("margin-left")) >= 0) && (parseInt($(avatar).css("margin-left")) <= 550)) {
+  $("body").keydown(function(event){
+    if ((parseInt($("#avatar").css("margin-left")) >= 0) && (parseInt($(avatar).css("margin-left")) <= 438)) {
       if(event.keyCode==39){
         playerPostion+=carManuvar
-        $(avatar).css("margin-left", playerPostion)
+        $("#avatar").css("margin-left", playerPostion)
       }else if(event.keyCode==37){
         playerPostion-=carManuvar
-        $(avatar).css("margin-left", playerPostion)
+        $("#avatar").css("margin-left", playerPostion)
       }
     }
   })
