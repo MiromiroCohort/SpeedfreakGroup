0ddbbc70638828b0ffa5ccf7e34440e291854c78
Lani Field <apprentice@StMac3-2.local>
2015-08-07 16:07:19 +1200

    about to add startscreen

--- scripts/app.js
+++ scripts/app.js
@@ -9,6 +9,7 @@
   var roadLineCount = 15
   var centerLineCount = 0
   var bonusPoints = 0
+  var shieled = false
 
   function Board(){
     $("body").append( $("<div class='board'>").css({ "height": "480px", "width": "640px", "background-color": "black", "margin": "auto", "z-index": "1"}) );
@@ -21,8 +22,9 @@
       <div class='road '></div>\
       <div class='side_line'></div>\
       <div class='grass'></div>\
-      <div class='car_slider'><img id='avatar' src='images/broombroomcar.png'></div> <br><br>");
-     $(".board").append("<h1 id='scoreboard'><br>Score:</h1>" );
+      <div class='car_slider'><img id='avatar' src='images/broombroomcar.png'></div> \
+      <br><br>");
+     $(".board").append("<h1 id='scoreboard'><br>Score:</h1>   <img src='images/shield.png' class='shield-status'>" );
 
 
     if(i%100==0){$(".side_line").append(
@@ -78,11 +80,12 @@ Board()
 
 
     if(spawnCount==0){
-        var imgArray = ["<img class='obstacle moving death crash' src='images/crash.png' style='margin-left:" + Math.floor((Math.random() * 400) + 30) + "px'>", "<img class='obstacle moving bonus' src='images/fwog.png' style='margin-left:" + Math.floor((Math.random() * 400) + 30) + "px'>", "<img class='obstacle moving death barrel' src='images/barrel.png' style='margin-left:" + Math.floor((Math.random() * 400) + 30) + "px'>"]
-        turlcheck = Math.floor(Math.random()*10)
-        console.log(turlcheck)
-        if(turlcheck == 5){
-          icon =  "<img class='obstacle moving bonus turle' src='images/turle.gif' style='margin-left:"+ Math.floor((Math.random() * 400) + 30) + "px'>"
+        var imgArray = ["<img class='obstacle moving death crash' src='images/crash.png' style='margin-left:" + Math.floor((Math.random() * 400) + 30) + "px'>", "<img class='obstacle moving bonus' src='images/fwog.png' style='margin-left:" + Math.floor((Math.random() * 400) + 30) + "px'>", "<img class='obstacle moving death barrel' src='images/barrel2.png' style='margin-left:" + Math.floor((Math.random() * 400) + 30) + "px'>"]
+        powerup = Math.floor(Math.random()*10)
+        // console.log(powerup)
+        if(powerup == 5){
+          powerupArray= ["<img class='obstacle moving bonus turle' src='images/turle.gif' style='margin-left:"+ Math.floor((Math.random() * 400) + 30) + "px'>", "<img class='obstacle moving bonus shield' src='images/shield.png' style='margin-left:"+ Math.floor((Math.random() * 400) + 30) + "px'>"]
+          var icon =  powerupArray[Math.floor(Math.random()*imgArray.length)]
         }else {
           var icon = imgArray[Math.floor(Math.random()*imgArray.length)]
         }
@@ -103,16 +106,27 @@ Board()
               $(this).removeClass("obstacle")
               if($(this).hasClass("turle")){
                 speedFactor = speedFactor - 1
+              }else if($(this).hasClass("shield")){
+                shieled = true
+                $(".shield-status").fadeIn()
+                console.log(shieled)
+              ($(".shield").css("display","inline-block"))
               }else{
                 speedFactor++
               }
-          }else {//alert("GAME OVER. score = " +parseInt((i/10) + bonusPoints))
 
+          } else {//alert("GAME OVER. score = " +parseInt((i/10) + bonusPoints))
+            if(shieled){
+              shieled = false
+              $(".shield-status").fadeOut()
+              $(this).remove()
+            }else{
               playerAlive = false
           }
 
         }
       }
+    }
     });
     spawnCount = spawnCount-1
     sideLineCount = sideLineCount-1
@@ -124,17 +138,19 @@ Board()
 
   doMove()
   $("body").keydown(function(event){
-    if ((parseInt($("#avatar").css("margin-left")) >= 0) && (parseInt($(avatar).css("margin-left")) <= 438)) {
-      if(event.keyCode==39){
-        playerPostion+=carManuvar
-        $("#avatar").css("margin-left", playerPostion)
-      }else if(event.keyCode==37){
-        playerPostion-=carManuvar
-        $("#avatar").css("margin-left", playerPostion)
+    if(playerAlive){
+      if((parseInt($("#avatar").css("margin-left")) >= 0) && (parseInt($(avatar).css("margin-left")) <= 438)) {
+        if(event.keyCode==39){
+          playerPostion+=carManuvar
+          $("#avatar").css("margin-left", playerPostion)
+        }else if(event.keyCode==37){
+          playerPostion-=carManuvar
+          $("#avatar").css("margin-left", playerPostion)
+        }
+      } else {
+        //alert("SPIN OUT. score = " +parseInt((i/10)))
+        playerAlive = false
       }
-    } else {
-      //alert("SPIN OUT. score = " +parseInt((i/10)))
-      playerAlive = false
     }
   })
 
